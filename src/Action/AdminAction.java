9accf91c3dd9c9724e48ea977e909e9eee1dfdd7
Joshioner <550942828@qq.com>
2018-04-06 15:46:31 +0800

    add a file

---
+++ src/Action/AdminAction.java
@@ -0,0 +1,44 @@
+package Action;
+
+import Entity.Admin;
+import Service.AdminService;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminAction {
+    private AdminService service;
+
+    @Resource(name = "adminService")
+    public void setService(AdminService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/login")
+    public String login(Admin admin, Model model, HttpSession session)
+    {
+        try {
+              Admin adminInfo = service.login(admin);
+            session.setAttribute("adminInfo",adminInfo);
+            return "redirect:/user/getAll.action";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "/error/error.jsp";
+        }
+        //创建sqlsession对象
+//        SqlSession sqlSession = sqlSessionFactory.openSession();
+//        //执行查询步骤
+//        sqlSession.selectOne("adminNamespace.login",admin);
+//        //关闭资源
+//        sqlSession.close();
+//        model.addAttribute("message","成功");
+//            return "/jsp/success.jsp";
+    }
+}
---
+++ src/Entity/User.java
@@ -0,0 +1,52 @@
+package Entity;
+
+/**
+ * 用户实体类
+ */
+public class User {
+    private int id;  //用户编号
+    private String name; //用户姓名
+    private String gender; //性别
+    private int age;    //年龄
+    private double salary; //薪水
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
